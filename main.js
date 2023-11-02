521f916450514bf5b7eda893f3ae6d1b2f605125
Thong Dino <hodinhthong.auto@gmail.com>
2023-11-02 11:34:37 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+// function myTimer() {
+//     const date = new Date();
+//     document.querySelector("h1").innerHTML = date.toLocaleTimeString();
+// };
+
+// setInterval(myTimer, 1000);
+
+//BUILD PROJECT BUT DON'T USE AVAILABLE FUNCTION, DONT USE SETINTERVAL TIMEOUT
+
+
+function displayClock () {
+    const d = new Date();
+
+    let h = d.getHours();
+    let section = h >= 0 && h < 12 ? "AM" : "PM";
+    h = (h >= 0 && h < 12 ? h : h - 12).toString().padStart(2, "0");
+    const m = d.getMinutes().toString().padStart(2, "0");
+    const s = d.getSeconds().toString().padStart(2, "0");
+    const clock = `${h}:${m}:${s} ${section}`;
+    const h1Text = document.querySelector("h1");
+    h1Text.innerHTML = clock;
+
+    setTimeout(displayClock, 1000)
+};
+
+displayClock();
+
+
